0352f53399d87012a370f1a65f813045cc587b44
AlexJavierQ <alexquishpe45@gmail.com>
2022-10-21 17:17:19 -0500

    carga inicial del proyecto

---
+++ src/Virtualizacion.java
@@ -0,0 +1,6 @@
+public class Virtualizacion {
+    public static void main(String[] args) {
+        System.out.println("        TEMAS DE PROYECTOS EMPLEADOS EN CENTRO DE CONVENCIONES");
+        System.out.println("");
+    }
+}
