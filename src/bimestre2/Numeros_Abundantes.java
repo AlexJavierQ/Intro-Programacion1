427308a9c66a7e3e735417219165063716a6e3e6
Alex Q <alexquishpe45@gmail.com>
2022-12-07 16:46:17 -0500

    Onta el sus

---
+++ src/bimestre2/Numeros_Abundantes.java
@@ -0,0 +1,30 @@
+package bimestre2;
+
+import java.util.Scanner;
+
+public class Numeros_Abundantes {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+       int a = 0;                 //Variables
+       int i = 1;
+
+       System.out.println("Digite un número");             //Pregunta
+        int n;
+        n = teclado.nextInt();
+
+        while(i < n) {                                //Contador
+
+            if (n % i == 0){                      // Proceso de division
+                a = a+i;
+            }
+            i ++;
+       }
+
+       if (a > n ){                                        //Detecta si s abundadnte o no
+           System.out.println("Es un número abundante");
+       }else{
+           System.out.println("No es un número abundante");
+       }
+    }
+}
+
